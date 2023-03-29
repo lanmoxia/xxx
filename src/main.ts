2588135e3380848d391806405baa1769570ab458
lanmoxia <506076395@qq.com>
2023-03-29 23:13:57 +0800

    初始化 Router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createWebHashHistory, createRouter } from 'vue-router'
 
-createApp(App).mount('#app')
+const history = createWebHashHistory()
+
+const router = createRouter({
+  history: history,
+  routes: []
+})
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
