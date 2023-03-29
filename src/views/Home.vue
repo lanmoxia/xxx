209b52a9fd35c0a902d2f657f98cf9c2e349b39a
lanmoxia <506076395@qq.com>
2023-03-30 00:43:33 +0800

    Home 页面样式完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,40 @@
       <h2>XXX-ui 框 架</h2>
       <p class="actions">
         <a href="#">GitHub</a>
-        <a href="#">开始</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
 </template>
+
 <script>
 import TopNav from '../components/TopNav.vue'
 export default {
   components: {TopNav}
 }
 </script>
+
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
