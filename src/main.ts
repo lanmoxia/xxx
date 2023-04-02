fcdbcc34c92c2b264a85a1fb6512aac0f6faa8dc
lanmoxia <506076395@qq.com>
2023-04-02 21:58:10 +0800

    作为 UI 库：样式最小影响原则

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
+// 要放到前边来
+import './lib/xxx.scss'
 import './index.scss'
+import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 
