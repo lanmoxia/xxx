81456e25d51cff3e634b5116137ada85dd656181
lanmoxia <506076395@qq.com>
2023-04-06 16:08:53 +0800

    Switch 完成开关功能
    Signed-off-by: lanmoxia <506076395@qq.com>

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="wrapper">
+    <section>
+      <h1>Switch 按钮</h1>
+      <p>开关选择器。</p>
+    </section>
+    <section>
+      <h2>应用场景</h2>
+      <p>需要在两种状态之间切换时。</p>
+    </section>
+    <section>
+      <h2>代码示例</h2>
+    </section>
+    <div>
+      <div class="demo">
+        <h3>常规用法</h3>
+        <div class="demo-component">
+          <Switch v-model:value="bool"/>
+        </div>
+        <div class="demo-actions">查看代码</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
+export default {
+  components: {Switch},
+  setup(){
+    const bool = ref(false)
+    return {bool}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  p {color: rgba(0, 0, 0, 0.65); font-size: 14px;}
+  h1, h2 {color: #0d1a26; margin-bottom: 20px;}
+  h3 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid #d9d9d9;
+  }
+  section {
+    padding-bottom: 20px;
+    > h1 {font-size: 30px; line-height: 38px;}
+    > h2 {font-size: 24px; line-height: 32px;}
+  }
+  .demo {
+    border: 1px solid #d9d9d9;
+    margin-bottom: 32px;
+    &-component {
+      padding: 16px;
+    }
+    &-actions {
+      padding: 8px 16px;
+      border-top: 1px dashed #d9d9d9;
+    }
+  }
+}
+</style>
